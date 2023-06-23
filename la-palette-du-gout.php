7624a1dcffb77f649c7f893747083c081643c722
Simon LM <simon.lm.dev@pm.me>
2023-06-23 11:08:23 +0200

    first back to html

--- la-palette-du-gout.php
+++
@@ -1,8 +0,0 @@
-<?php include "./objects/header-with-arrow.php" ?>
-
-    <?php include "./objects/restaurants/img-top_menus-restaurants.php" ?>
-    <main class="menu-restaurants menu-restaurants-1">
-    <?php include "./objects/restaurants/main_menus-restaurants.php" ?>
-    </main>
-
-<?php include "./objects/footer.php" ?>
